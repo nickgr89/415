70131c1e9f6345ab32fbb56ec3ac5ac0dd7505f2
Marcus Little <marclouisiana@yahoo.com>
2016-02-23 17:30:02 -0600

    installed moq

--- EnterpriseSystems/UnitTests.Data/Mappers/AppointmentMapperTest.cs
+++ EnterpriseSystems/UnitTests.Data/Mappers/AppointmentMapperTest.cs
@@ -1,22 +1,38 @@
-﻿using EnterpriseSystems.Data.Mappers;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EnterpriseSystems.Data.Mappers;
+using EnterpriseSystems.Data.DAO;
+using EnterpriseSystems.Data.Hydraters;
+using EnterpriseSystems.Data.Model.Entities;
+using Moq;
 
 namespace UnitTests.Data.Mappers
 {
+    [TestClass]
     public class AppointmentMapperTest
     {
         private AppointmentMapper Target { get; set; }
+        private Mock<IDatabase> MockDatabase { get; set; }
+        private Mock<IQuery> MockQuery { get; set; }
+        Mock<IHydrater<AppointmentVO>> MockAppointmentHydrater { get; set; }
 
         [TestInitialize]
         public void TestInitialize()
         {
-            Target = new AppointmentMapper();
+            MockDatabase = new Mock<IDatabase>();
+            MockQuery = new Mock<IQuery>();
+            MockAppointmentHydrater = new Mock<IHydrater<AppointmentVO>>();
+            Target = new AppointmentMapper(MockDatabase.Object, MockAppointmentHydrater.Object);
         }
 
         [TestCleanup]
         public void TestCleanup()
         {
             Target = null;
+            MockDatabase = null;
+            MockQuery = null;
+            MockAppointmentHydrater = null;
         }
+
+        //[TestMethod]
     }
 }
