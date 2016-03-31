becd607781418922f5370d13ed8b9fdcec556e0e
Nicholas Granat <nicholas.granat@selu.edu>
2016-03-31 15:01:52 -0500

    1/9 tests passing

--- EnterpriseSystems/EnterpriseSystems.Domain/Model/Order/OrderFactory.cs
+++ EnterpriseSystems/EnterpriseSystems.Domain/Model/Order/OrderFactory.cs
@@ -4,9 +4,24 @@ using System.Linq;
 using EnterpriseSystems.Data.Model.Entities;
 
 namespace EnterpriseSystems.Domain.Model.Order
-{    
+{
     public class OrderFactory : IOrderFactory
     {
-        
+        private static OrderFactory instance;
+
+        public static OrderFactory GetInstance()
+        {
+
+                if (instance == null) {
+                    instance = new OrderFactory();
+                }
+                return instance;            
+
+        }
+
+        public Order Create(CustomerRequestVO customerRequest)
+        {
+            return ((IOrderFactory)instance).Create(customerRequest);
+        }
     }
 }
