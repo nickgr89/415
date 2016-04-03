1027723e4dfa20a3c09fa5eeeb43b928fda31b5b
Nicholas Granat <nicholas.granat@selu.edu>
2016-04-03 16:08:31 -0500

    2/9 tests passing

--- EnterpriseSystems/EnterpriseSystems.Domain/Model/Order/OrderFactory.cs
+++ EnterpriseSystems/EnterpriseSystems.Domain/Model/Order/OrderFactory.cs
@@ -10,11 +10,7 @@ namespace EnterpriseSystems.Domain.Model.Order
         private static OrderFactory _order;
 
 
-
-
-
-
-        // test 1/9
+        // test 1/9, GetInstance_ReturnsSingletonInstance
         public static OrderFactory GetInstance()
         {
                 if (_order == null) {
@@ -27,13 +23,55 @@ namespace EnterpriseSystems.Domain.Model.Order
         {
             var _order = new Order();
 
-            // test 2/9
-            var result = from refNum 
+            // test 2/9, Create_SetsOrderNumberFromBillOfLading
+            var orderNum = (from refNum 
                 in customerRequest.ReferenceNumbers
                 where (refNum.ReferenceNumberType == ReferenceNumberTypes.BillOfLading)
-                select refNum.ReferenceNumber;
-            _order.OrderNumber = result;
+                select refNum.ReferenceNumber).First();
+            _order.OrderNumber = new OrderNumber(orderNum);
 
+
+            // test 3/9 Create_SetsOriginFromFirstStop
+            var origin = (from stop
+                in customerRequest.Stops
+                /*where (stop.StopNumber == 1)*/
+                select stop).First();
+
+
+
+            /*_order.Origin.Name = origin.OrganizationName;
+            _order.Origin.Address.AddressLines[0] = origin.AddressLine1;
+            _order.Origin.Address.AddressLines.[1] = origin.AddressLine2;
+            _order.Origin.Address.City = origin.AddressCityName;
+            _order.Origin.Address.StateCode = origin.AddressStateCode;
+            _order.Origin.Address.PostalCode = origin.AddressPostalCode;
+
+
+            // test 4/9, Create_SetsDestinationFromLastStop
+            var destination = from stop
+                in customerRequest.Stops
+                where (stop.StopNumber == 2)
+                select stop;
+            _order.Origin.Name = destination.OrganizationName;
+            _order.Origin.Address.AddressLines[0] = destination.AddressLine1;
+            _order.Origin.Address.AddressLines[1] = destination.AddressLine2;
+            _order.Origin.Address.City = destination.AddressCityName;
+            _order.Origin.Address.StateCode = destination.AddressStateCode;
+            _order.Origin.Address.PostalCode = destination.AddressPostalCode;
+
+
+            // test 5/9, Create_SetsWorkTypeFromConsumerClassificationType
+            var consumer = customerRequest.ConsumerClassificationType;
+            if (consumer == "HOMEDELVR")
+            {
+                _order.WorkType = WorkType.Home;
+            }else if (consumer == "BLDRDIRECT" || consumer == "BLDRINDRCT")
+            {
+                _order.WorkType = WorkType.Builder;
+            }else
+            {
+                _order.WorkType = WorkType.Retail;
+            }*/
 
 
 
